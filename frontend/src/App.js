25ec7ec95d873411326295c5191b05a62ca0759d
Mackewinsson <mackewinsson@gmail.com>
2020-10-20 16:34:48 -0300

    secord commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container, Row, Col } from "react-bootstrap";
 
 const App = () => {
   return (
     <>
-      <h1>Bienvenido a Bellas Cejas Academy</h1>
+      <Header />
+      <main>
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="mt-5">Bienvenido a Bellas Cejas Academy</h1>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
