6b1486440d93b712e8377e6b249830b2c06f36af
Mackewinsson <mackewinsson@gmail.com>
2020-11-07 12:39:23 -0300

    colors added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
 const app = express();
-const products = require("./data/products");
+import products from "./data/products.js";
+import connectDB from "./config/db.js";
+dotenv.config();
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is runing");
@@ -15,4 +20,10 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server running in [${process.env.NODE_ENV}] mode on port [${PORT}]`
+  )
+);
