f1ad064a6b87d97f28cc42b13b6c4f11eb966a9f
Mackewinsson <mackewinsson@gmail.com>
2020-11-07 11:37:10 -0300

    Simple backend done

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,29 @@
-import React from "react";
-import products from "../products";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-
-// Map over fake data
-const productRender = products.map((product) => {
-  return (
-    <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-      <Product product={product} />
-    </Col>
-  );
-});
+import axios from "axios";
 
 const Homescreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
+  // Map over data
+  const productRender = products.map((product) => {
+    return (
+      <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+        <Product product={product} />
+      </Col>
+    );
+  });
+  // End maping over data
+
   return (
     <>
       <h1>Latest Products</h1>
